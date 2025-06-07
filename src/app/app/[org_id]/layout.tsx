f6e4d74d52de5e3793b5149cbcc9453c6a3eb314
Jonathan Killian <jonathankurtis@gmail.com>
2025-06-07 01:02:07 -0400

    feat: add dynamic breadcrumbs

--- src/app/app/[org_id]/layout.tsx
+++ src/app/app/[org_id]/layout.tsx
@@ -1,12 +1,5 @@
 import { AppSidebar } from '@/components/app-sidebar';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
+import Breadcrumbs from '@/components/breadcrumbs';
 import { Separator } from '@/components/ui/separator';
 import {
   SidebarInset,
@@ -33,19 +26,7 @@ export default async function Page({
               orientation='vertical'
               className='mr-2 data-[orientation=vertical]:h-4'
             />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className='hidden md:block'>
-                  <BreadcrumbLink href='#'>
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className='hidden md:block' />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+            <Breadcrumbs />
           </div>
         </header>
         <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
