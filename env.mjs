cdcc4b53aea32945b5b0df97d895346ac103e4f2
jonkurtis <jonathankurtis@gmail.com>
2025-06-16 23:21:26 -0400

    add t3 env

---
+++ env.mjs
@@ -0,0 +1,37 @@
+// src/env.mjs
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    SERVICE_ROLE: z.string().min(1),
+    EMAIL_API_KEY: z.string().min(1),
+    EMAIL_PROVIDER: z.enum(['ses', 'resend']).default('ses'),
+  },
+  /*
+   * Environment variables available on the client (and server).
+   *
+   * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   */
+  client: {
+    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
+  },
+  /*
+   * Due to how Next.js bundles environment variables on Edge and Client,
+   * we need to manually destructure them to make sure all are included in bundle.
+   *
+   * 💡 You'll get type errors if not all variables from `server` & `client` are included here.
+   */
+  runtimeEnv: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    SERVICE_ROLE: process.env.SERVICE_ROLE,
+    EMAIL_API_KEY: process.env.EMAIL_API_KEY,
+    EMAIL_PROVIDER: process.env.EMAIL_PROVIDER,
+  },
+});
